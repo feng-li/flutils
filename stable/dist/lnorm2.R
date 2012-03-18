4e122b08c75b82ce94402b3896573a83cea12549
Feng Li <m@feng.li>
2012-03-18 17:23:38 +0100

    Update the documentation for vech2m function

--- stable/dist/lnorm2.R
+++ stable/dist/lnorm2.R
@@ -1,1 +1,45 @@
 ##' Alternative parametrization of log normal distribution.
+##'
+##' See help("rlnorm") for the details for the log-normal distribution. 
+##' @title Log-normal distribution with alternative parametrization.
+##' @param mean "vector" the mean value of the log-normal distribution. 
+##' @param sd "vector" the variance of the log-normal distribution.
+##' @return See the corresponding help for the usual log-normal functions. 
+##' @references Li Villani Kohn 2010.
+##' @author Feng Li, Department of Statistics, Stockholm University, Sweden.
+##' @note Created: Fri Mar 16 17:59:11 CET 2012;
+##'       Current: Fri Mar 16 17:59:18 CET 2012.
+rlnorm2 <- function(n, mean, sd)
+  {
+    var <- sd^2
+    s2 <- log(var/mean^2+1)
+    m <- log(mean) - s2/2
+    out <- rlnorm(n = n, meanlog = m, sdlog = sqrt(s2))
+    return(out)
+  }
+dlnorm2 <- function(x, mean, sd, log = FALSE)
+  {
+    var <- sd^2
+    s2 <- log(var/mean^2+1)
+    m <- log(mean) - s2/2
+    out <- dlnorm(x = x, meanlog = m, sdlog = sqrt(s2), log = log)
+    return(out)
+  }
+plnorm2 <- function(q, mean, sd, lower.tail = TRUE, log.p = FALSE)
+  {
+    var <- sd^2
+    s2 <- log(var/mean^2+1)
+    m <- log(mean) - s2/2
+    out <- plnorm(q = q, meanlog = m, sdlog = sqrt(s2),
+                  lower.tail=lower.tail, log.p=log.p)
+    return(out)
+  }
+qlnorm2 <- function(p, mean, sd, lower.tail = TRUE, log.p = FALSE)
+  {
+    var <- sd^2
+    s2 <- log(var/mean^2+1)
+    m <- log(mean) - s2/2
+    out <- qlnorm(p = p, meanlog = m, sdlog = sqrt(s2),
+                  lower.tail=lower.tail, log.p=log.p)
+    return(out)
+  }
--- stable/others/vech2m.R
+++ stable/others/vech2m.R
@@ -1,7 +1,7 @@
-##' <description>
+##' Convert from the vech vector to the full matrix.
 ##'
-##' <details>
-##' @title <short tile>
+##' 
+##' @title Vech to matrix
 ##' @param vech 
 ##' @param diag 
 ##' @return 
@@ -9,14 +9,14 @@
 ##' @author Feng Li, Department of Statistics, Stockholm University, Sweden.
 ##' @note
 ##'     Created: Wed Oct 12 20:23:17 CEST 2011;
-##'     Current: Wed Oct 12 20:23:51 CEST 2011.
+##'     Current: Sun Mar 18 17:07:11 CET 2012.
 vech2m <- function(vech, diag = TRUE)
 {
   c <- length(vech)
   p <- (1 - 2*diag +sqrt(8*c+1))/2 # n^2 -n  =  2c; or n^2 + n  =  2c
   if(!identical(p,  round(p)))
     {
-      stop ("Input vector length not match with length of matrix triangular.")
+      stop ("Input vector length not match with the length of matrix triangular.")
     }
   out <- matrix(1, p, p)
   idx.lower <- as.vector(lower.tri(out, diag = diag))
