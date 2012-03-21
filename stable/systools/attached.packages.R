42f42357760c109c54d6811d14bf731d82000654
Feng Li <m@feng.li>
2012-03-21 15:45:07 +0100

    Add a function to check which function is attached in the R environment.

---
+++ stable/systools/attached.packages.R
@@ -0,0 +1,17 @@
+##' List all attached packages
+##'
+##' 
+##' @title list all attached packages
+##' @return 
+##' @references 
+##' @author Feng Li, Department of Statistics, Stockholm University, Sweden.
+##' @note Created: Wed Mar 21 12:01:21 CET 2012;
+##'       Current: Wed Mar 21 12:01:27 CET 2012.
+attached.packages <- function()
+{
+  searchOut <- search()
+  loadedNMS <- loadedNamespaces()
+  out <- searchOut[searchOut%in%paste("package:", loadedNMS ,sep="")]
+  return(out)
+}
+
