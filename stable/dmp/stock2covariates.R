daa1c4e63668cd6a283a8453f942d18c0d11e2d9
Feng Li <m@feng.li>
2012-04-15 16:58:31 +0200

    Fix a underflow

--- stable/dmp/stock2covariates.R
+++ stable/dmp/stock2covariates.R
@@ -1,7 +1,7 @@
 ##' Construct sensible covariates for stock market data.
 ##'
 ##' The historical finance data can be obtained from "Yahoo! Finance".
-##' @title Construct time series covariates. 
+##' @title Construct time series covariates.
 ##' @param file "character".
 ##'        The location of the CSV format file from Yahoo!  Finance which can
 ##'        be the http address.
@@ -20,12 +20,13 @@
 stock2covariates <- function(file, g = c(0.95, 0.80),
                              kappa = 1, ma = c(1, 5, 20))
   {
-    ## Read the CSV file from disk and subtract the raw covariates 
-    DataRaw <- read.csv(file, sep = ",")
+    ## Read the CSV file from disk and subtract the raw covariates
+    ## TODO: the comma of the csv file makes troubles.
+    DataRaw <- read.csv(file, sep = ",", header=TRUE)
     nObs <- nrow(DataRaw) # Number of observations
 
     ## The Yahoo csv format was ordered with time descending order. Reorder it
-    ## so that the first observation is from the oldest time.  
+    ## so that the first observation is from the oldest time.
     Data <- DataRaw[nObs:1, , drop = FALSE]
 
     Close = Data[,"Close"]
@@ -41,21 +42,23 @@ stock2covariates <- function(file, g = c(0.95, 0.80),
                           paste("CloseSqrt", g*100, sep = "")))
     MinMaxAbs <- matrix(NA, nObs, length(g),
                         dimnames = list(NULL,
-                          paste("MaxAbs", g*100, sep = "")))
+                          paste("MinMaxAbs", g*100, sep = "")))
     RMA <- matrix(NA, nObs, length(ma),
                   dimnames = list(NULL,
                     paste("RMA", ma, sep = "")))
-    
+
     ## The Returns
     ## 100log(p(t)/p(t-1)), where p(t) is closing price
     Close1 <- c(NA, Close[1:(nObs-1)])
-    Returns = 100*log(Close/Close1) 
-    tInit <- 2 # The initial point by taking away the first lag.
-    tIdx <- (max(ma, 3)+2):nObs 
+    Returns <- 100*(log(Close)-log(Close1))
 
-    ## Loop over all the observations and construct the covariates. NOTE: This
+    ## Time indices
+    tInit <- 2 # The initial point by taking away the first lag.
+    tIdx <- (max(ma, 3)+2):nObs
+
+    ## Loop over all the observations to construct the covariates. NOTE: This
     ## is the utility function which does not require very high
-    ## efficiency--loops are ok at the moment.
+    ## efficiency--loops are OK at the moment.
     for(t in tIdx)
       {
         for(i in 1:length(ma))
@@ -69,12 +72,13 @@ stock2covariates <- function(file, g = c(0.95, 0.80),
             CloseSqrt[t, j] <- sqrt((1-g[j])*
                                     sum(g.s*(Returns[tInit:(t-2)]^2)^kappa))
             MinMaxAbs[t, j] <- (1-g[j])*
-              sum(g.s*(100*log(High[tInit:(t-2)]/Low[tInit:(t-2)]))^kappa)
+              sum(g.s*(100*(log(High[tInit:(t-2)])-log(Low[tInit:(t-2)])))^kappa)
           }
       }
-    outData <- cbind(RMA, CloseAbs, CloseSqrt, MinMaxAbs)[tIdx, , drop = FALSE]
+    outData <- cbind(Returns, RMA, CloseAbs, CloseSqrt, MinMaxAbs)[tIdx, , drop = FALSE]
     Date <- as.Date(Data[tIdx, "Date"], "%Y-%m-%d")
-      
+
+    browser()
     out <- data.frame(Date, outData)
     return(out)
   }
