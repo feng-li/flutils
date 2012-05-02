da5dedd1a63b95f3d910aa9b5f78873a04e78dbd
Feng Li <m@feng.li>
2012-05-03 00:35:37 +0200

    Minor changes

--- stable/splines/make.knots.R
+++ stable/splines/make.knots.R
@@ -1,40 +1,40 @@
 ##' Setup knots for spline models.
-##' 
+##'
 ##' This function can be used in the initial values for splines
 ##' @name make.knots
 ##' @title Locate the knots in m-space.
-##' 
+##'
 ##' @param x "matrix".
 ##'         n-by-m. Covariates matrix *without* intercept.
 ##' @param n.knots "integer".
-##'         Number of knots used. 
+##'         Number of knots used.
 ##' @param method "character".
-##'         Method to be use in the knots locating method. Currently value are "k-means", 
+##'         Method to be use in the knots locating method. Currently value are "k-means",
 ##'         "mahalanobis-eball" which came from Villani et al (2009) and "es" for equal
 ##'         spaced sample quantile with single covariate and "random".
 ##' @param args "list".
 ##'         Other arguments need to pass to the function w.r.t different "method". When
 ##'         method is "mahalanobis-eball", you need to provide:
-##'         args$RadiusShrink: "numeric", the radus shrinkage for the ball. 
-##' 
+##'         args$RadiusShrink: "numeric", the radus shrinkage for the ball.
+##'
 ##' @return "list".
 ##'         A list with knots locations for given numbers of knots.
-##' 
+##'
 ##' @references Appendix C. in Villani et al (2009)
 ##' @author Feng Li, Department of Statistics, Stockholm University, Sweden.
-##' 
+##'
 ##' @note First version: Wed Mar 10 14:03:31  CET 2010;
 ##'       Current:       Thu Sep 16 13:56:37 CEST 2010.
-##' TODO: 
+##' TODO:
 make.knots <- function(x, method, splineArgs)
 {
   out <- list()
   ks <- splineArgs$thinplate.s.dim[1]
-  
+
   if(tolower(method) == "no-knots") # ad-hoc for regression without knots
     {
       ## if("thinplate.s"  %in% splineArgs$comp)
-      ##   {  
+      ##   {
           out[["thinplate.s"]] <- matrix(NA, ks, dim(x)[2])
       ##   }
       ## if("thinplate.a"  %in% splineArgs$comp)
@@ -42,7 +42,7 @@ make.knots <- function(x, method, splineArgs)
           out[["thinplate.a"]] <- matrix(NA, sum(splineArgs$thinplate.a.locate), 1)
         ## }
 
-    }     
+    }
   else if(tolower(method) == "k-means")
     {
       iter.max <- 200
@@ -63,18 +63,18 @@ make.knots <- function(x, method, splineArgs)
         {
           a.locate <- splineArgs$thinplate.a.locate
           ## x.idx4knots <- rep(1:m, a.locate)
-         
+
           x.noempty <- x[, a.locate != 0, drop = FALSE]
           x.noempty.list <- array2list(x.noempty, 2)
           nknots <- a.locate[a.locate != 0]
           location <- mapply(x.noempty.list, FUN = function(x, centers) kmeans(x, centers,
-                                               iter.max = iter.max)$centers, centers = as.list(nknots))   
+                                               iter.max = iter.max)$centers, centers = as.list(nknots))
           out[["thinplate.a"]] <- matrix(unlist(location))
-        }      
-      
+        }
+
     }
   else if(tolower(method) == "random") # TODO: Maybe a good consideration if no. of knots
-                                        # exceed no. of obs. 
+                                        # exceed no. of obs.
     {
       if("thinplate.s"  %in% splineArgs$comp)
         {
@@ -97,8 +97,8 @@ make.knots <- function(x, method, splineArgs)
                                                 nknots)], nknots = as.list(nknots))
           out[["thinplate.a"]] <- matrix(unlist(location))
         }
-    }  
-  ## else if(tolower(method)=="equal-spaced") 
+    }
+  ## else if(tolower(method)=="equal-spaced")
   ##   {
   ##     ## Equal spaced sample quantile for single covariate.
   ##         ## TODO: make this for multivariate covariates.
