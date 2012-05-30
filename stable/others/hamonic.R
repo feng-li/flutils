a746c45990ced6df60f19835f92b1ad9b428a8db
Feng Li <m@feng.li>
2012-05-30 10:52:41 +0200

    Include the harmonic function

---
+++ stable/others/hamonic.R
@@ -0,0 +1,14 @@
+##' Harmonic number
+##'
+##'
+##' @title Harmonic number
+##' @param n "numeric" Non integer is also allowed.
+##' @return "numeric"
+##' @author Feng Li, Department of Statistics, Stockholm University, Sweden.
+##' @note Created: Wed May 30 10:51:32 CEST 2012;
+##'       Current: Wed May 30 10:51:37 CEST 2012.
+harmonic <- function(n)
+  {
+    out <- digamma(n+1) - digamma(1)
+    return(out)
+  }
