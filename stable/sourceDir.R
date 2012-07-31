8cbb13369dad0b9b34befaf283c379333f352dcd
Feng Li <m@feng.li>
2012-07-31 11:06:39 +0200

    Make byte compile be FALSE by default

--- stable/sourceDir.R
+++ stable/sourceDir.R
@@ -5,7 +5,7 @@
 ##' @param byte.compile "logical". If TRUE, byte compile the R code first and load. Else,
 ##'        only load R code.
 ##' @param recursive "logical". If TRUE, files will be sourced recursively for all
-##'        sub folders. 
+##'        sub folders.
 ##' @param silent "logical". If TRUE, No detailed information printed out.
 ##' @param ignore.error "logical". If TRUE, try to continue even errors occur.
 ##' @return NULL
@@ -14,14 +14,14 @@
 ##' @note First version: Wed Apr 15 20:00:43 CET 2009;
 ##'       Current:       Sat Nov 12 15:42:54 CET 2011.
 ##' TODO: add environmental option, allow to only source byte code.
-sourceDir <- function(..., byte.compile = TRUE, recursive = TRUE, silent = FALSE,
-                      ignore.error = FALSE) 
+sourceDir <- function(..., byte.compile = FALSE, recursive = TRUE, silent = FALSE,
+                      ignore.error = FALSE)
 {
   ## call the path
   Paths <- c(...)
-  if(length(Paths) == 0) # if path is not given,  use current working directory 
+  if(length(Paths) == 0) # if path is not given,  use current working directory
     {
-      Paths <- getwd() 
+      Paths <- getwd()
     }
   ## Take away the extra separator "\" or "/"
   Paths <- gsub(paste("\\",.Platform$file.sep,"$",sep=""),"",Paths)
@@ -52,14 +52,14 @@ sourceDir <- function(..., byte.compile = TRUE, recursive = TRUE, silent = FALSE
         {
           warning(paste("\"", Paths[i], "\": no such directory, skipped.", sep = ""))
         }
-      else 
+      else
         {
           if(silent == FALSE)
             {
               cat("\n\"", Paths[i], "/\":\n", sep = "")
             }
         }
-      for (nm in subfiles) ## 
+      for (nm in subfiles) ##
         {
           n <- n + 1
           path.nm <- file.path(Paths[i], nm)
@@ -69,21 +69,21 @@ sourceDir <- function(..., byte.compile = TRUE, recursive = TRUE, silent = FALSE
               path2source.nm <- paste(gsub("\\.[Rr]$", "", path.nm), ".Rc", sep = "")
               if(methods::is(compile.try, "try-error") || file.exists(path2source.nm) == FALSE) # non-function file can not be compiled.
                 {
-                  source(path.nm) # try to use source() directly 
+                  source(path.nm) # try to use source() directly
                 }
               else # load compiled file
                 {
                   loadcmp(path2source.nm)
                 }
             }
-          else # the usual source procedure. 
-            {              
+          else # the usual source procedure.
+            {
               if(ignore.error == FALSE) # stop at error
                 {
                   if(silent == FALSE) # report results
                     {
                       source(path.nm)
-                      cat(paste("   \"", nm, "\"", " ... [ok]\n", sep = "")) 
+                      cat(paste("   \"", nm, "\"", " ... [ok]\n", sep = ""))
                     }
                   else # silent if no error
                     {
@@ -97,29 +97,29 @@ sourceDir <- function(..., byte.compile = TRUE, recursive = TRUE, silent = FALSE
                     {
                       if(methods::is(try.source, "try-error"))
                         {
-                          cat(paste("   \"", nm, "\"", " ... [??]\n", sep = "")) 
+                          cat(paste("   \"", nm, "\"", " ... [??]\n", sep = ""))
 
                           ## cat(format(c(paste("...\"", nm, "\"", sep = ""), "[??]"), width
-                          ##            = 65), "\n") 
+                          ##            = 65), "\n")
                           error <- error + 1
                         }
                       else
                         {
-                          cat(paste("   \"", nm, "\"", " ... [ok]\n", sep = "")) 
+                          cat(paste("   \"", nm, "\"", " ... [ok]\n", sep = ""))
                         }
                     }
-                  else # silent 
+                  else # silent
                     {
                       if(methods::is(try.source, "try-error"))
                         {
                           error <- error + 1
                         }
-                    } 
-                } 
+                    }
+                }
             }
         }
- }     
-  ## Summary on quit 
+ }
+  ## Summary on quit
   if(silent  == FALSE)
     {
       cat(format(paste("[Succeeded files: ",  n - error, ",", " skipped due to error: ",
