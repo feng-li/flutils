91ece867dba5bf3834e1069c07c524715eb75fc2
Feng Li <m@feng.li>
2012-10-06 18:53:41 +0200

    A major revision of sourceDir() function which includes the following changes
    + Allow mixture of directors and files as the file input
    + Add pattern as an argument for the input files
    + Only load byte compiled file if it exists and newer than the source file
    + Byte compile file to temporal directory if the source director is not
      writable
    + All source file into specified environment
    + Loops replaced by using *apply functions
    
    TODO:
    + More tests.
    + Think about "namespace" to make it more like a package-style thing
    + Incorporate with parallel package to make the function as simple parallel
      interface for executing multiple R files (byte compile does not make sense
      here)

--- stable/sourceDir.R
+++ stable/sourceDir.R
@@ -6,7 +6,9 @@
 ##'        only load R code.
 ##' @param recursive "logical". If TRUE, files will be sourced recursively for all
 ##'        sub folders.
+##' @param envir "environment" What is destiny of the files to be sourced.
 ##' @param silent "logical". If TRUE, No detailed information printed out.
+##' @param pattern "string". What kind of file pattern is sourced
 ##' @param ignore.error "logical". If TRUE, try to continue even errors occur.
 ##' @return NULL
 ##' @author Feng Li, Department of Statistics, Stockholm University, Sweden.
@@ -14,115 +16,201 @@
 ##' @note First version: Wed Apr 15 20:00:43 CET 2009;
 ##'       Current:       Sat Nov 12 15:42:54 CET 2011.
 ##' TODO: add environmental option, allow to only source byte code.
-sourceDir <- function(..., byte.compile = FALSE, recursive = TRUE, silent = FALSE,
+##'       allow parallel souring
+##'
+sourceDir <- function(...,
+                      byte.compile = FALSE,
+                      recursive = FALSE,
+                      envir = .GlobalEnv,
+                      silent = FALSE,
+                      pattern = "\\.[Rr]$",
                       ignore.error = FALSE)
 {
-  ## call the path
-  Paths <- c(...)
-  if(length(Paths) == 0) # if path is not given,  use current working directory
+  ## Save all the input paths as a list
+  Paths.in <- list(...)
+
+  ## if path is not given,  use current working directory
+  if(length(Paths.in) == 0)
     {
-      Paths <- getwd()
+      Paths.lst <- list(getwd())
     }
-  ## Take away the extra separator "\" or "/"
-  Paths <- gsub(paste("\\",.Platform$file.sep,"$",sep=""),"",Paths)
+
+  ## Check if all inputs are directories. The inputs can also mixture of
+  ## directories and files. If so,  split them
+  isPathsDir <- unlist(lapply(Paths.in,
+                              FUN = function(x) file_test("-d", x)))
+
+  if(any(isPathsDir))
+    {
+      Paths.lst <- Paths.in[isPathsDir]
+      Paths.files <- unlist(Paths.in[!isPathsDir])
+
+      ## list all subfiles for given pattern at Paths
+      RFiles <- rbind(matrix(unlist(lapply(X = Paths.lst,
+                                           FUN = list.files,
+                                           pattern = pattern,
+                                           recursive = recursive,
+                                           full.names = TRUE))),
+                      Paths.files)
+    }
+  else
+    {
+      ## All are files
+      RFiles <- matrix(unlist(Paths.in))
+    }
+
+
   ## Check if byte compile is requested and supported.
   if(byte.compile == TRUE)
     {
-      if(version$minor >= 13)
+      if(version$major >= 2 && version$minor >= 13)
         {
           require("compiler") # require the compiler library
         }
       else
         {
           byte.compile == FALSE
-          warning("Byte code compiling not support on this version!")
+          warning("Byte compiling not supported, use usual method.")
         }
     }
-  n <- 0
-  error <- 0
-  if(silent == FALSE)
+
+
+  ## Check if the corresponding compiled files exist
+  if(byte.compile == TRUE)
     {
-      ## cat("Loading files  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n")
-    }
-  for(i in 1:length(Paths)) # loop over different paths
-    {
-      subfiles <- list.files(Paths[i], pattern = "\\.[Rr]$", recursive = recursive) # list
-                                        # all subfiles for given pattern at Paths[i]
-      if(length(subfiles) == 0) # unknown folder
+      ## Function that find the modified time of a file
+      file.mtime <- function(x) {file.info(x)$mtime}
+
+      ## Get the modification information from the list files
+      RFilesInfo <- matrix(apply(RFiles,1,file.mtime))
+
+      RcFilesTest <- matrix(apply(RFiles,1,function(x) paste(x, "c", sep = "")))
+
+      RcFilesExist <- matrix(apply(RcFilesTest,1,file.exists))
+
+      RcFile.mtime <- function(fileTest, fileExist)
         {
-          warning(paste("\"", Paths[i], "\": no such directory, skipped.", sep = ""))
+          ifelse(fileExist, file.info(fileTest)$mtime,  0)
+        }
+
+      RcFilesInfo <- mapply(FUN = RcFile.mtime,
+                            fileTest = RcFilesTest,
+                            fileExist = RcFilesExist,
+                            USE.NAMES = FALSE)
+
+      ## Check if R file is newer than Rc file
+      RcNewer <- (RcFilesInfo > RFilesInfo)
+    }
+  else
+    {
+      ## Auxiliary information
+      RcNewer <- NA
+      RcFilesExist <- NA
+      RcFilesTest <- NA
+    }
+
+###----------------------------------------------------------------------------
+### Write a private source function
+###----------------------------------------------------------------------------
+  source.prvt <- function(path2R,
+                          path2Rc = NA,
+                          is.RcNewer = NA,
+                          is.RcExist = NA,
+                          byte.compile,
+                          envir,
+                          ignore.error)
+    {
+      ## The initial status
+      success <- TRUE
+
+      if(byte.compile == FALSE) # the usual source procedure.
+        {
+          if(ignore.error == FALSE) # stop on error
+            {
+              sys.source(path2R, envir = envir)
+            }
+          else # skip error files
+            {
+              source.try <- try(sys.source(path2R, envir = envir),
+                                silent = TRUE)
+              if(methods::is(source.try,  "try-error"))
+                {
+                  success <- FALSE
+                }
+            }
+        }
+      else # byte compile before sourcing
+        {
+          ## If Rc file exists and is newer than R file,  load Rc file
+          ## directly. Otherwise byte compile R file and load it.
+          if(is.RcExist && is.RcNewer)
+            {
+              RcOfile <- path2Rc
+            }
+          else
+            {
+              ## Check if the folder is writable, if so, save the compiled file
+              ## there. Otherwise, save it in a temporal directory
+              isWritable <- file.create(path2Rc, showWarnings = FALSE)
+              if(isWritable)
+                {
+                  ## It's OK to write in the same directory
+                  RcOfile <- path2Rc
+                }
+              else
+                {
+                  ## The original path not writable, write to temporal file
+                  path2RcStr <- gsub(pattern = .Platform$file.sep,
+                                  replacement = "_",
+                                  x = path2Rc)
+                  RcOfile <- paste(tempdir(), .Platform$file.sep,
+                                   path2RcStr, sep = "")
+                }
+
+              ## Byte compile
+              compRc.try <- try(cmpfile(infile = path2R,
+                                         outfile = RcOfile,
+                                         env = envir), silent = TRUE)
+            }
+
+          source.try <- try(loadcmp(RcOfile, envir = envir),
+                            silent = TRUE)
+
+          if(methods::is(source.try,  "try-error"))
+                {
+                  success <- FALSE
+                }
+
+        }
+
+      return(success)
+    }
+
+###----------------------------------------------------------------------------
+### Apply the source function to the files and output
+###----------------------------------------------------------------------------
+  sourceSucess <- mapply(FUN = source.prvt,
+                         path2R = RFiles,
+                         path2Rc = RcFilesTest,
+                         is.RcNewer = RcNewer,
+                         is.RcExist = RcFilesExist,
+                         MoreArgs = list(
+                           byte.compile = byte.compile,
+                           envir = envir,
+                           ignore.error = ignore.error))
+
+
+  ## Return status if required
+  if(!silent)
+    {
+      if(byte.compile)
+        {
+          out <- data.frame(RFiles, RcFilesExist, RcNewer, sourceSucess)
         }
       else
         {
-          if(silent == FALSE)
-            {
-              cat("\n\"", Paths[i], "/\":\n", sep = "")
-            }
+          out <- data.frame(RFiles, sourceSucess)
         }
-      for (nm in subfiles) ##
-        {
-          n <- n + 1
-          path.nm <- file.path(Paths[i], nm)
-          if(byte.compile == TRUE)
-            {
-              compile.try <- try(cmpfile(path.nm), silent = TRUE)
-              path2source.nm <- paste(gsub("\\.[Rr]$", "", path.nm), ".Rc", sep = "")
-              if(methods::is(compile.try, "try-error") || file.exists(path2source.nm) == FALSE) # non-function file can not be compiled.
-                {
-                  source(path.nm) # try to use source() directly
-                }
-              else # load compiled file
-                {
-                  loadcmp(path2source.nm)
-                }
-            }
-          else # the usual source procedure.
-            {
-              if(ignore.error == FALSE) # stop at error
-                {
-                  if(silent == FALSE) # report results
-                    {
-                      source(path.nm)
-                      cat(paste("   \"", nm, "\"", " ... [ok]\n", sep = ""))
-                    }
-                  else # silent if no error
-                    {
-                      source(path.nm)
-                    }
-                }
-              else # skip error files
-                {
-                  try.source <- try(source(path.nm), silent = TRUE)
-                  if(silent == FALSE) # report results
-                    {
-                      if(methods::is(try.source, "try-error"))
-                        {
-                          cat(paste("   \"", nm, "\"", " ... [??]\n", sep = ""))
-
-                          ## cat(format(c(paste("...\"", nm, "\"", sep = ""), "[??]"), width
-                          ##            = 65), "\n")
-                          error <- error + 1
-                        }
-                      else
-                        {
-                          cat(paste("   \"", nm, "\"", " ... [ok]\n", sep = ""))
-                        }
-                    }
-                  else # silent
-                    {
-                      if(methods::is(try.source, "try-error"))
-                        {
-                          error <- error + 1
-                        }
-                    }
-                }
-            }
-        }
- }
-  ## Summary on quit
-  if(silent  == FALSE)
-    {
-      cat(format(paste("[Succeeded files: ",  n - error, ",", " skipped due to error: ",
-                       error, ".]\n\n", sep = ""), justify = "centre", width = 65), "\n")
+      return(out)
     }
 }
