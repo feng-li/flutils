787ca45bc1983015e572e16d87382a6447902abb
Feng Li <m@feng.li>
2012-10-17 20:06:19 +0200

    Add a simple hexadecimal string generator.

---
+++ others/rhex.R
@@ -0,0 +1,18 @@
+##' A simple hexadecimal random string generator
+##'
+##'
+##' @title Random hex
+##' @param n "positive-integer"
+##' @return "string"
+##' @author Feng Li, Department of Statistics, Stockholm University, Sweden.
+##' @note Created: Wed Oct 17 20:04:58 CEST 2012;
+##'       Current: Wed Oct 17 20:05:05 CEST 2012.
+rhex <- function(n)
+  {
+    hexStr <- c("0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+                "a", "b", "c", "d", "e", "f")
+
+    hexRnd <- sample(hexStr, n, replace = TRUE)
+    out <- paste(hexRnd, collapse = "")
+    return(out)
+  }
