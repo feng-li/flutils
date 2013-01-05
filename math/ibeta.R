9c2f5da46dbb6af2dbb2b2d72352c0b057d68028
Feng Li <m@feng.li>
2013-01-05 22:52:25 +0100

    Add option to allow regularized incomplete beta function.

--- math/ibeta.R
+++ math/ibeta.R
@@ -1,26 +1,39 @@
 ##' This function is used to compute the incomplete beta function.
 ##'
-##' 
+##'
 ##' @title Incomplete beta function
-##' @param x 
-##' @param a 
-##' @param b 
-##' @param log 
-##' @return 
-##' @references 
+##' @param x
+##' @param a
+##' @param b
+##' @param log
+##' @param reg If TRUE,  return the regularized incomplete beta function. Same
+##' as the Mathematica definition.
+##'
+##' @return
+##' @references
 ##' @author Feng Li, Department of Statistics, Stockholm University, Sweden.
 ##' @note Created: Wed Oct 26 18:39:12 CEST 2011;
 ##'       Current: Wed Feb 08 20:08:25 CET 2012.
 ##' TODO: let -1 < b < oO.
-ibeta <- function(x, a, b, log = FALSE)
+ibeta <- function(x, a, b, log = FALSE, reg = FALSE)
 {
   HCond <- (a >0 && b>0)
-  if(!HCond) 
+  if(!HCond)
     {
       stop("Incomplete beta function requires a > 0 and b > 0.")
     }
-  
-  out.log <- pbeta(x, a, b,  log = TRUE) + lbeta(a, b)
+
+  ibeta.log <- pbeta(x, a, b,  log = TRUE) + lbeta(a, b)
+
+  if(reg == TRUE)
+    {
+      out.log <- ibeta.log - log(beta(a = a, b = b))
+    }
+  else
+    {
+      out.log <- ibeta.log
+    }
+
 
   if(log == TRUE)
     {
@@ -30,6 +43,6 @@ ibeta <- function(x, a, b, log = FALSE)
     {
       out <- exp(out.log)
     }
-  
+
   return(out)
 }
