bd1e0c0368b6a5fca5cd3ccbe652e0ddeb592a11
Feng Li <m@feng.li>
2013-01-07 21:01:00 +0100

    Rewrite the CDF function as the form of Li et al 2010.

--- dist/splitt.R
+++ dist/splitt.R
@@ -18,9 +18,9 @@ dsplitt <- function(x, mu, df, phi, lmd, log)
     I0 <- (x <= mu) # Logical values. 1, if y <= mu; 0, if y >mu.
     I <- (!I0)  # Logical values. 1, if y > mu; 0, if y <= mu.
 
-    Sign <- 1*I0 + lmd^2*I # Sign = 1 if y<=mu; Sign = lmd.^2 if y>2
+    sign <- 1*I0 + lmd*I # sign = 1 if y<=mu; sign = lmd.^2 if y>2
 
-    density.log <- (df/(df+(-mu+x)^2/phi^2/Sign))^((1+df)/2)/
+    density.log <- (df/(df+(-mu+x)^2/phi^2/sign^2))^((1+df)/2)/
       phi/sqrt(df)/beta(df/2,1/2)/(1+lmd)*2
 
     if(log == TRUE)
@@ -39,33 +39,13 @@ psplitt <- function(x, mu, df, phi, lmd, log)
     ## CDF for y < = mu part.
     I0 <- (x<=mu)
     I <- (!I0)
+    sign <- I0*1 + I*lmd
+    sign2 <- I0*(-1) + I*1
 
-    out <- x
-    out[0:length(x)] <- NA
-
-    if(length(I0) != 0L)
-      {
-        A0 = df[I0]/(df[I0]+(x[I0]-mu[I0])^2/phi[I0]^2)
-
-        BetaRegularized0 = 1- ibeta(x = A0,a = df(I0)/2,b = 1/2,
-          log = FALSE, reg = TRUE)
-
-        density0 = (1 - BetaRegularized0)/(1+lmd[I0])
-        out[I0]= density0
-      }
-
-    if(length(I) != 0L)
-      {
-        ## CDF for y > mu part.
-        A = df[I]/(df[I]+(x[I]-mu[I])^2/phi[I]^2/lmd[I]^2)
-        BetaRegularized = 1- ibeta(x = A,a = df(I)/2,b = 1/2,
-          log = FALSE, reg = TRUE)
-
-        density1 = (1 - lmd[I] + lmd[I]*(1+ BetaRegularized))/(1+lmd[I])
-
-        out[I]=density1
-      }
-
+    A <- df*sign^2*phi^2/(df*sign^2*phi^2+(x-mu)^2)
+    BetaRegUpper <- 1- ibeta(x = A,a = df/2,b = 1/2,
+                                log = FALSE, reg = TRUE)
+    out <- 1/(1+lmd) + sign*sign2/(1+lmd)*BetaRegUpper
 
     if(log == TRUE)
       {
@@ -102,8 +82,6 @@ splitt.skewness <- function(df, phi, lmd)
     skewness <- m3/var^(3/2)
     return(skewness)
   }
-
-
 
 splitt.kurtosis <- function(df, phi, lmd)
   {
