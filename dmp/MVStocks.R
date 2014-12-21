26f5caf8bf4a139becfc115d1f4283c041873ac2
Feng Li <m@feng.li>
2014-12-21 03:39:51 -0500

    Function to download Stock Data from Yahoo and Convert it into model friendly format.

---
+++ dmp/MVStocks.R
@@ -0,0 +1,97 @@
+##'
+##'
+##' Download and build multivariate responses and covariates from Yahoo Finance.
+##' @param from integer format as YYYYMMDD
+##'
+##' @param to integer format as YYYYMMDD
+##'
+##' @param stocks a vector of characters. The aberrations are same as stocks used in
+##' Yahoo! Finance.
+##'
+##' @param StdDataMethod character. Whether the data should be standardized.
+##'
+##' @param save2diskPath Is not missing, the result will be saved to the path provided
+##' here.
+##'
+##' @return list or save to disk.
+##'
+##' @references Li Villani 2012
+##'
+##' @author Feng Li, Central University of Finance and Economics.
+
+MVStocks <- function(from, to, stocks = c("^SML", "^OEX"),
+                    StdDataMethod= "norm-0-1", save2diskPath)
+{
+
+    ## Format the URL in Yahoo URL Format as
+    ## http://real-chart.finance.yahoo.com/table.csv?s = %5ESML&a = 07&b = 2&c = 1982&d = 11&e = 21&f = 2014&g = d&ignore = .csv
+    a <- substr(from, 7, 8)
+    b <- as.numeric(substr(from, 5, 6))-1
+    c <- substr(from, 1, 4)
+    d <- substr(to, 7, 8)
+    e <- as.numeric(substr(to, 5, 6))-1
+    f <- substr(to, 1, 4)
+
+    g <- "d" ## daily
+
+    ## THE SOURCE OF DATA
+    RawDataPath <- list()
+    for(s in stocks)
+        {
+            RawDataPath[[s]] <- paste("http://real-chart.finance.yahoo.com/table.csv?s=",s,
+                                      "&a=",a,"&b=",b,"&c=",c,
+                                      "&d=",d,"&e=",e,"&f=",f,
+                                      "&g=",g,"&ignore=.csv", sep = "")
+        }
+
+
+    ## Convert the raw data into covariates
+    Scovar <- lapply(X = RawDataPath, FUN = stock2covariates)
+
+    XRaw <- list()
+    YRaw <- list()
+
+    ## Extract common IDs. Due to technical reasons, the stocks are not available for some
+    ## days. We extract the common days.
+
+    ScovarID <- lapply(Scovar, function(x)x[["ID"]])
+    ID <- Reduce(intersect, ScovarID)
+
+    for(i in names(Scovar))
+        {
+            CovCurr <- Scovar[[i]]
+            nCol <- ncol(CovCurr)
+            XRaw[[i]] <- as.matrix(CovCurr[ID, 3:nCol, drop = FALSE])
+            YRaw[[i]] <- as.matrix(CovCurr[ID, "Returns"])
+        }
+
+    ## Standardize the data
+    if(!missing(StdDataMethod))
+        {
+            XNew <- lapply(XRaw, StdData, method = StdDataMethod)
+            YNew <- lapply(YRaw, StdData, method = StdDataMethod)
+
+            X <- lapply(XNew, function(x) x$data)
+            Y <- lapply(YNew, function(x) x$data)
+            X.config <- lapply(XNew, function(x) x$config)
+            Y.config <- lapply(YNew, function(x) x$config)
+        }
+    else
+        {
+            X <- XRaw
+            Y <- YRaw
+            X.config <- NA
+            Y.config <- NA
+        }
+
+    ## Save to file
+    if(!missing(save2disk))
+        {
+            save(ID, X, Y,  X.config, Y.config, file = save2diskPath)
+        }
+    else
+        {
+            return(list(ID, X, Y, X.config, Y.config))
+        }
+
+}
