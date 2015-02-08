511fa6d57070b10b1cc5d15bdff49445d690af00
Feng Li <m@feng.li>
2015-02-08 17:05:42 +0800

    Minor updates

--- mcmc/parMeanFun.R
+++ mcmc/parMeanFun.R
@@ -11,50 +11,53 @@
 ##' @note Created: Sun Mar 04 14:45:22 CET 2012;
 ##'       Current: Sun Mar 04 14:45:29 CET 2012.
 parMeanFun <- function(X, beta, linkArgs)
-  {
-    ## Input each observation x'b  -> l(phi) = x'b -> phi
-    beta <- matrix(beta)
-    linPred <- X %*% beta # The linear predictor
+    {
+        ## Input each observation x'b  -> l(phi) = x'b -> phi
+        ## We allow for beta to be a matrix of (p-by-lq)
+        p <- dim(X)[2]
+        beta <- matrix(beta, nrow = p)
 
-    link <- linkArgs[["type"]]
+        linPred <- X %*% beta # The linear predictor
 
-    if(tolower(link) %in% "identity")
-      {
-        out <- linPred
-      }
-    else if(tolower(link) %in% c("log", "glog"))
-      {
-        if(tolower(link) == "glog")
-          {
-            a <- linkArgs$a
-          }
+        link <- linkArgs[["type"]]
+
+        if(tolower(link) %in% "identity")
+            {
+                out <- linPred
+            }
+        else if(tolower(link) %in% c("log", "glog"))
+            {
+                if(tolower(link) == "glog")
+                    {
+                        a <- linkArgs$a
+                    }
+                else
+                    {
+                        a <- 0
+                    }
+
+                out <- exp(linPred) + a
+            }
+        else if(tolower(link) %in% c("glogit", "logit"))
+            {
+                if(tolower(link) == "logit")
+                    {
+                        a <- 0
+                        b <- 1
+                    }
+                else
+                    {
+                        a <- linkArgs$a
+                        b <- linkArgs$b
+                    }
+
+                ## out <- 1/(1+exp(-linPred))
+                out <- a + (b-a)/(1+exp(-linPred))
+            }
         else
-          {
-            a <- 0
-          }
+            {
+                stop("This link function is not implemented yet!")
+            }
 
-        out <- exp(linPred) + a
-      }
-    else if(tolower(link) %in% c("glogit", "logit"))
-      {
-        if(tolower(link) == "logit")
-          {
-            a <- 0
-            b <- 1
-          }
-        else
-          {
-            a <- linkArgs$a
-            b <- linkArgs$b
-          }
-
-        ## out <- 1/(1+exp(-linPred))
-        out <- a + (b-a)/(1+exp(-linPred))
-      }
-    else
-      {
-        stop("This link function is not implemented yet!")
-      }
-
-    return(out)
-  }
+        return(out)
+    }
