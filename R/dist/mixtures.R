5f4b7c4ef66ec4d37085c31af438726d501936f1
Feng Li <m@feng.li>
2017-11-27 19:24:54 +0800

    Add mixture mean and variacne

--- R/dist/mixtures.R
+++ R/dist/mixtures.R
@@ -74,7 +74,53 @@ mixture.mean <- function(type, par.list)
     if(tolower(type) == "splitt")
     {
 
-    }
+        mixture.mu = par.list[["mu"]]
+        mixture.df = par.list[["df"]]
+        mixture.phi = par.list[["phi"]]
+        mixture.lmd = par.list[["lmd"]]
+        mixture.weights = par.list[["weights"]]
 
+        nComp <- ncol(mixture.weights)
+
+        mixture.mean <- apply(matrix(1:nComp), 1, function(comp)
+            splitt.mean(mu = mixture.mu[, comp],
+                        df = mixture.df[, comp],
+                        phi = mixture.phi[, comp],
+                        lmd = mixture.lmd[, comp]))
+
+        out <- rowSums(mixture.mean*mixture.weights)
+    }
+    return(out)
+}
+
+mixture.var <- function(type, par.list)
+{
+    ## Sylvia Frühwirth-Schnatter, Finite Mixture and Markov Switching Models
+    if(tolower(type) == "splitt")
+    {
+
+        mixture.mu = par.list[["mu"]]
+        mixture.df = par.list[["df"]]
+        mixture.phi = par.list[["phi"]]
+        mixture.lmd = par.list[["lmd"]]
+        mixture.weights = par.list[["weights"]]
+
+        nComp <- ncol(mixture.weights)
+
+        mixture.mean <- apply(matrix(1:nComp), 1, function(comp)
+            splitt.mean(mu = mixture.mu[, comp],
+                        df = mixture.df[, comp],
+                        phi = mixture.phi[, comp],
+                        lmd = mixture.lmd[, comp]))
+
+        mixture.vars <- apply(matrix(1:nComp), 1, function(comp)
+            splitt.var(mu = mixture.mu[, comp],
+                       df = mixture.df[, comp],
+                       phi = mixture.phi[, comp],
+                       lmd = mixture.lmd[, comp]))
+
+        out <- (rowSums((mixture.mean^2+mixture.vars)*mixture.weights) -
+                rowSums(mixture.mean*mixture.weights)^2)
+    }
     return(out)
 }
